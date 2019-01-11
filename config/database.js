1792fd2440c198d6f8b344cbcd14bccde3a1f1b8
jeswsi <james@woodruffscientific.com>
2019-01-11 12:15:26 -0800

    Changed DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    // remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'todo:todotutorial@ec2-52-91-121-96.compute-1.amazonaws.com:8000/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
